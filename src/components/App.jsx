837b353f9da0c9f9afebc53d680e7afdb5436eba
yesroh <ronaizacard@gmail.com>
2017-04-20 09:10:23 -0300

    Adding index and our fist componet

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="title">
+          Reminder Pro
+        </div>
+        <div className="form-inline">
+          <div className="form-group">
+            <input
+              className="form-control"
+              placeholder="I have to..."
+            />
+          </div>
+          <button className="btn btn-success" type="button">Add Reminder</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
